86c2608a2fde32b5fc796f55642ccee0c19c9ef8
Raquel Leite <raquel.rleite@outlook.com>
2025-09-07 20:22:18 -0300

    primeiro commit

--- src/test/java/com/raquel/cadastrodeninjas/CadastroDeNinjasApplicationTests.java
+++ src/test/java/com/raquel/cadastrodeninjas/CadastroDeNinjasApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CadastroDeNinjasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
