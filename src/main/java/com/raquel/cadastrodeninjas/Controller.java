3bf6bcb2287702ebb1b4dbfb677a4e6d5a9b589e
Raquel Leite <raquel.rleite@outlook.com>
2025-09-07 21:30:29 -0300

    Criado a controller

---
+++ src/main/java/com/raquel/cadastrodeninjas/Controller.java
@@ -0,0 +1,18 @@
+package com.raquel.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Controlador para API rest
+@RequestMapping //Colocar todas as nossas rotas no mesmo lugar
+public class Controller {
+
+    @GetMapping ("/boasVindas")//Pega informações, no caso, ta pegando essa mensagem de boas vindas, essa é a rota, colcoa / pra separar, se for colocar uma rota dentro de uma rota, colona / no final tambem
+    public String welcome(){
+        return "Seja bem-vindo a primeira rota desta API.";
+    }
+
+
+
+}
