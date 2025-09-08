da7d7dd47132bd41dac3b7d5727db8c6edf38672
Raquel Leite <raquel.rleite@outlook.com>
2025-09-07 22:24:31 -0300

    Criado uma entidade no banco de dados

--- src/main/java/com/raquel/cadastrodeninjas/NinjaController.java
+++ src/main/java/com/raquel/cadastrodeninjas/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Controlador para API rest
 @RequestMapping //Colocar todas as nossas rotas no mesmo lugar
-public class Controller {
+public class NinjaController {
 
     @GetMapping ("/boasVindas")//Pega informações, no caso, ta pegando essa mensagem de boas vindas, essa é a rota, colcoa / pra separar, se for colocar uma rota dentro de uma rota, colona / no final tambem
     public String welcome(){
